4e022a737e6f453036830fd024a69bfdceed5453
mosecoza <mosecoza@gmail.com>
2024-05-13 19:27:11 +0200

    chore: use FibrePlan in its pages

--- app/fibreplane/page.tsx
+++ app/fibreplane/page.tsx
@@ -1,11 +1,12 @@
-"use client"
+"use client";
 import Image from "next/image";
 import type { NextPage } from "next";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FibrePlan } from "../../src/components/FibrePlan";
+import { fibrePlans } from "../../src/utils/constants";
 
-
-const Fibreplane : NextPage = () => {
+const FibrePlans: NextPage = () => {
   useEffect(() => {
     const fetData = async () => {
       const request = await fetch(`/api/portal`);
@@ -42,10 +43,6 @@ const Fibreplane : NextPage = () => {
       </div>
       <section style={{ backgroundColor: "#cccccc14", padding: "25px" }}>
         <div>
-          <br />
-          <br />
-          <br />
-          <br />
           <h1
             style={{
               textAlign: "center",
@@ -59,271 +56,22 @@ const Fibreplane : NextPage = () => {
           </h1>
           <br />
           <div className="content1">
-            <div className="basic box">
-              <h1 className="title">
-                <b>10 Mbp</b>
-              </h1>
-              <div className="view">
-                <div className="cost">
-                  <p className="amount">R350</p>
-                </div>
-                <hr />
-                <br />
-                <br />
-              </div>
-              <div className="description">
-                <ul>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Installation.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Activation Fee:
-                    R399
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Total Month 1:
-                    R749
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> From Month 2:
-                    R350
-                  </li>
-                </ul>
-              </div>
-              <div className="button4">
-                <button onClick={() => push("/ten-megabytes")} type="submit">
-                  <h4 style={{ paddingLeft: "48px", paddingTop: "9px" }}>
-                    SIGN UP NOW
-                  </h4>
-                </button>
-              </div>
-            </div>
-            <div className="standard box">
-              <h2 className="title">
-                <b>20 Mbp</b>
-              </h2>
-              <div className="view">
-                <div className="cost">
-                  <p className="amount">R599</p>
-                </div>
-                <hr />
-                <br />
-                <br />
-              </div>
-              <div className="description">
-                <ul>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Installation.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Connection.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Activation Fee:
-                    R399.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Total Month 1:
-                    R898.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> From Month 2:
-                    R599
-                  </li>
-                </ul>
-              </div>
-              <div className="button4">
-                <button onClick={() => push("/twenty-megabytes")} type="submit">
-                  <h4 style={{ paddingLeft: "48px", paddingTop: "9px" }}>
-                    SIGN UP NOW
-                  </h4>
-                </button>
-              </div>
-            </div>
-            <div className="business box">
-              <h2 className="title">
-                <b>30 Mbps</b>
-              </h2>
-              <div className="view">
-                <div className="cost">
-                  <p className="amount">R899</p>
-                </div>
-                <hr />
-                <br />
-                <br />
-              </div>
-              <div className="description">
-                <ul>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Installation.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Connection.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Activation Fee:
-                    R399.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Total Month 1:
-                    R1098.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> From Month 2:
-                    R899.
-                  </li>
-                </ul>
-              </div>
-
-              <div className="button4">
-                <button onClick={() => push("/thirty-megabytes")} type="submit">
-                  <h4 style={{ paddingLeft: "48px", paddingTop: "9px" }}>
-                    SIGN UP NOW
-                  </h4>
-                </button>
-              </div>
-            </div>
-            <div className="business box">
-              <h2 className="title">
-                <b>50 Mbps</b>
-              </h2>
-              <div className="view">
-                <div className="cost">
-                  <p className="amount">R999</p>
-                </div>
-                <hr />
-                <br />
-                <br />
-              </div>
-              <div className="description">
-                <ul>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Installation.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Connection.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Activation
-                    Feee: R399.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Total Month 1:
-                    R1898
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> From Month 2:
-                    R999.
-                  </li>
-                </ul>
-              </div>
-              <div className="button4">
-                <button onClick={() => push("/fifty-megabytes")} type="submit">
-                  <h4 style={{ paddingLeft: "48px", paddingTop: "9px" }}>
-                    SIGN UP NOW
-                  </h4>
-                </button>
-              </div>
-            </div>
-            <div className="business box">
-              <h2 className="title">
-                <b>100 Mbp</b>
-              </h2>
-              <div className="view">
-                <div className="cost">
-                  <p className="amount">R1199</p>
-                </div>
-                <hr />
-                <br />
-                <br />
-              </div>
-              <div className="description">
-                <ul>
-                  <li>
-                    {" "}
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Installation.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free Router.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Free
-                    Connection.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Activation Fee:
-                    R999.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> Total Month 1:
-                    R2198.
-                  </li>
-                  <li>
-                    <b style={{ color: "#222155" }}>&#x2022;</b> From Month 2:
-                    R1199.
-                  </li>
-                </ul>
-              </div>
-
-              <div className="button4">
-                <button
-                  onClick={() => push("/hundred-megabytes")}
-                  type="submit"
-                >
-                  <h4 style={{ paddingLeft: "48px", paddingTop: "9px" }}>
-                    SIGN UP NOW
-                  </h4>
-                </button>
-              </div>
-            </div>
+            {fibrePlans.map((plan) => (
+              <FibrePlan key={plan.size} {...plan} />
+            ))}
           </div>
         </div>
       </section>
-      <br />
-      <br />
-      <br />
-      <br /> <br />
-      <br />
-      <br />
-      <br />
+
       <section className="showcase">
-        <Image width={925} height={151} alt="" src="/images/FTTH_SA-Banner-Image-Free-installation-1.png" />
+        <Image
+          width={925}
+          height={151}
+          alt=""
+          src="/images/FTTH_SA-Banner-Image-Free-installation-1.png"
+        />
       </section>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      
     </main>
   );
 };
-export default Fibreplane;
+export default FibrePlans;
